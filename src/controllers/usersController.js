9a00573065d9d3ab749d9abc431974c239778856
shunjid <shunjid.rahman@gmail.com>
2021-06-13 16:06:29 +0600

    Refactor user codebase to async-await

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,30 +1,37 @@
 const User = require("../models/usersModel");
 const { success, error } = require("../utils/response");
 
-const signUp = (req, res) => {
+const signUp = async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => success(res, user, 201))
-    .catch((e) => error(res, e.message, 501));
+  try {
+    await user.save();
+    success(res, user, 201);
+  } catch (e) {
+    error(res, e.message, 501);
+  }
 };
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => success(res, users, 200))
-    .catch((e) => error(res, e.message, 500));
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    success(res, users, 200);
+  } catch (e) {
+    error(res, e.message, 500);
+  }
 };
 
-const getUserById = (req, res) => {
+const getUserById = async (req, res) => {
   const { id: _id } = req.params;
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) error(res, "User not found", 404);
-      else success(res, user, 200);
-    })
-    .catch((e) => error(res, e.message, 500));
+  try {
+    const user = User.findById(_id);
+
+    if (!user) error(res, "User not found", 404);
+    else success(res, user, 200);
+  } catch (e) {
+    error(res, e.message, 500);
+  }
 };
 
 module.exports = { signUp, getUsers, getUserById };

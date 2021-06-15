6fd09d14fecfddd5426ad05a7a90052edfe96c16
shunjid <shunjid.rahman@gmail.com>
2021-06-15 15:38:45 +0600

    Hide password and token from user response

--- models/usersModel.js
+++ models/usersModel.js
@@ -83,6 +83,16 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
